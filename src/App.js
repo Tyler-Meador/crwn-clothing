7c721ae2eea29cd53d814339ae1c65413eff9f66
Tyler Meador <tmeador13@gmail.com>
2021-11-28 11:58:44 -0600

    Base sign in page added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import Login from "./pages/login/login.component";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={Login} />
       </Switch>
     </div>
   );
