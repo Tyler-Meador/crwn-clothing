48ce72cd9936e3e6e38093791e5a8fac13fd2ec5
Tyler Meador <tmeador13@gmail.com>
2021-12-04 00:20:21 -0600

    added collection pages and components

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,22 @@
 import { combineReducers } from "redux";
 import UserReducer from "./user/user.reducer";
 import CartReducer from "./cart/cart.reducer";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
+import DirectoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: UserReducer,
   cart: CartReducer,
+  directory: DirectoryReducer,
+  shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
